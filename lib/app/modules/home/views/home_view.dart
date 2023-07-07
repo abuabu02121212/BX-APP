b15770eba76bd6289f83fd5dc4c69090839c55b2
henry <henry@gmail.com>
2023-07-08 07:53:39 +0800

    首页游戏类型

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -39,30 +39,89 @@ class HomeView extends GetView<HomeController> {
                   radius: 0,
                 ),
               ),
-              Container(
-                width: double.infinity,
-                margin: EdgeInsets.only(top: 40.w),
-                height: 60.w,
-                decoration: BoxDecoration(color: const Color(0xff011A51), borderRadius: BorderRadius.circular(30.w)),
-                child: Row(
-                  children: [
-                    SizedBox(width: 20.w),
-                    Image.asset("assets/images/i-notice.webp", width: 36.w),
-                    Expanded(
-                        child: Container(
-                      padding: EdgeInsets.only(left: 20.w, right: 20.w),
-                      child: SingleScrollViewMarquee(
-                        text:
-                            "Bem-vindo ao paraíso dos jogos, preparamos uma variedade de jogos populares para você ter uma experiência de jogo diferente.",
-                        style: TextStyle(color: const Color(0xffffffff), fontSize: 22.w),
-                      ),
-                    )),
-                  ],
-                ),
-              )
+              const HomeMarquee(),
+              const HomeGameTypesWidget(),
+              SizedBox(height: 35.w),
+              Image.asset("assets/images/index-title1.webp", height: 83.w),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HomeGameTypesWidget extends StatelessWidget {
+  static const List<String> gameTypes = ["Quente", "Dentro \nDe Casa", "Slot", "Pesca", "Pôquer", "Esporte", "Ao Vivo", "Esports"];
+
+  const HomeGameTypesWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 148.w,
+      margin: EdgeInsets.only(top: 24.w, left: 20.w, right: 20.w),
+      decoration: BoxDecoration(
+        gradient: headerLinearGradient,
+        borderRadius: BorderRadius.circular(20.w),
+      ),
+      child: GridView.builder(
+          itemCount: 8,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, childAspectRatio: 2.4),
+          itemBuilder: (BuildContext context, int index) {
+            return CupertinoButton(
+              onPressed: () {
+                Toast.show("$index");
+              },
+              minSize: 0,
+              padding: EdgeInsets.zero,
+              child: Container(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset("assets/images/game-tab$index.webp", width: 50.w),
+                    Text(
+                      gameTypes[index],
+                      style: TextStyle(color: Colors.white, fontSize: 26.w),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class HomeMarquee extends StatelessWidget {
+  const HomeMarquee({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(top: 40.w, left: 20.w, right: 20.w),
+      height: 60.w,
+      decoration: BoxDecoration(color: const Color(0xff011A51), borderRadius: BorderRadius.circular(30.w)),
+      child: Row(
+        children: [
+          SizedBox(width: 20.w),
+          Image.asset("assets/images/i-notice.webp", width: 36.w),
+          Expanded(
+              child: Container(
+            padding: EdgeInsets.only(left: 20.w, right: 20.w),
+            child: SingleScrollViewMarquee(
+              text: "Bem-vindo ao paraíso dos jogos, preparamos uma variedade de jogos populares para você ter uma experiência de jogo diferente.",
+              style: TextStyle(color: const Color(0xffffffff), fontSize: 22.w),
+            ),
+          )),
+        ],
       ),
     );
   }
