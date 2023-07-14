b2f8243426f4f180c555645546bb1f4b18bf7dc7
henry <henry@gmail.com>
2023-07-14 18:49:10 +0800

    全部接口初步校对加入

--- lib/http/api.dart
+++ lib/http/api.dart
@@ -13,3 +13,23 @@ const String gameList = "/member/game/list";
 const String hotGameList = "/member/game/hot/list";
 const String forgetPsw = "/member/password/forget";
 
+const String lastwin = "/member/lastwin";
+const String notice = "/member/notice";
+const String vips = "/member/vips";
+const String signConfig = "/member/sign/config";
+const String signRewardRecord = "/member/sign/reward/record";
+const String sign = "/member/sign";
+const String messageList = "/member/message/list";
+const String messageRead = "/member/message/read";
+const String messageNum = "/member/message/num";
+const String messageDelete = "/member/message/delete";
+const String payChannel = "/pay/channel";
+const String withdrawConfig = "/pay/withdraw/config";
+const String payDeposit = "/pay/deposit";
+const String memberRecord = "/member/record";
+const String payWithdraw = "/pay/withdraw";
+const String treasureConfig = "/member/treasure/config";
+const String treasureApply = "/member/treasure/apply";
+const String promoDepositConfig = "/member/promo/deposit/config";
+
+
