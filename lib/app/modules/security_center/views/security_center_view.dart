ae0cd38009b6ee36a52ffcb950c6bb351937f9cd
henry <henry@gmail.com>
2023-07-25 04:51:11 +0800

    安全中心-静态UI

--- lib/app/modules/security_center/views/security_center_view.dart
+++ lib/app/modules/security_center/views/security_center_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_comm/app/routes/app_pages.dart';
+import 'package:flutter_comm/util/toast_util.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
@@ -15,26 +17,23 @@ class SecurityCenterView extends GetView<SecurityCenterController> {
     return Scaffold(
       backgroundColor: const Color(0xff020a1c),
       appBar: AppBar(
-        title: AppBar(
-          titleSpacing: 0,
-          leadingWidth: 0,
-          toolbarHeight: 110.w,
-          title: const AppHeader(title: "Centro de Segurança"),
-        ),
-        centerTitle: true,
+        titleSpacing: 0,
+        leadingWidth: 0,
+        toolbarHeight: 110.w,
+        title: const AppHeader(title: "Centro de Segurança"),
       ),
       body: SafeArea(
         child: Container(
           margin: EdgeInsets.only(left: 45.w, right: 45.w, top: 45.w),
-          decoration: const BoxDecoration(color: Color(0xff1a478e)),
+          decoration: BoxDecoration(color: const Color(0xff1a478e), borderRadius: BorderRadius.circular(20.w)),
           child: ListView(
-            children: [
-              ItemWidget(),
-              ItemWidget(),
-              ItemWidget(),
-              ItemWidget(),
-              ItemWidget(),
-            ],
+            shrinkWrap: true,
+            children: List.generate(
+                5,
+                (index) => ItemWidget(
+                      index: index,
+                      isLast: index == 4,
+                    )),
           ),
         ),
       ),
@@ -42,77 +41,135 @@ class SecurityCenterView extends GetView<SecurityCenterController> {
   }
 }
 
-class ItemWidget extends StatelessWidget {
-  const ItemWidget({
+class Line extends StatelessWidget {
+  const Line({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
+    return Container(
+      width: double.infinity,
+      height: 2.w,
+      color: const Color.fromRGBO(255, 255, 255, 0.25),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  ItemWidget({
+    super.key,
+    required this.index,
+    required this.isLast,
+  });
+
+  final int index;
+  final bool isLast;
+  final List<String> titleList = [
+    "Informações pessoais",
+    "Alterar senha de entrar",
+    "Senha de pagamento",
+    "Número da conta bancária",
+    "Verificação de telefone",
+  ];
+  final List<String> textList = [
+    "Informações pessoais completas.",
+    "Combinação recomendada de letras \ne números",
+    "Definir a senha de pagamento para melhorar a segurança da sua conta",
+    "Adicionar número de conta bancária",
+    "Verificação de telefone",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       children: [
-        Image.asset(
-          "assets/images/securiy_center_left_1.webp",
-          width: 90.w,
+        CupertinoButton(
+          onPressed: () {
+            if (index == 0) {
+              Get.toNamed(Routes.CENTER_USER_INFO);
+            } else if (index == 1) {
+              Get.toNamed(Routes.CENTER_PAY_PASSWORD);
+            } else if (index == 2) {
+              Get.toNamed(Routes.CENTER_BANK_LIST);
+            } else if (index == 3) {
+              Get.toNamed(Routes.CENTER_BANK_LIST_ADD);
+            } else if (index == 4) {
+              Get.toNamed(Routes.CENTER_PHONE);
+            }
+          },
+          minSize: 0,
+          padding: EdgeInsets.zero,
+          child: Container(
+            padding: EdgeInsets.only(top: 40.w, bottom: 32.w, left: 14.w, right: 14.w),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/securiy_center_left_${index + 1}.webp",
+                  width: 90.w,
+                ),
+                SizedBox(width: 10.w),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        titleList[index],
+                        style: TextStyle(fontSize: 28.w, color: Colors.white, fontWeight: FontWeight.w400),
+                      ),
+                      SizedBox(height: 6.w),
+                      Text(
+                        textList[index],
+                        maxLines: 3,
+                        style: TextStyle(fontSize: 24.w, color: const Color.fromRGBO(255, 255, 255, 0.70), fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CupertinoButton(
+                      onPressed: () {},
+                      minSize: 0,
+                      padding: EdgeInsets.all(10.w),
+                      child: Image.asset(
+                        "assets/images/security_center_right_ok.webp",
+                        width: 32.w,
+                      ),
+                    ),
+                    CupertinoButton(
+                      onPressed: () {},
+                      minSize: 0,
+                      padding: EdgeInsets.all(10.w),
+                      child: Image.asset(
+                        "assets/images/edit-1.webp",
+                        width: 32.w,
+                      ),
+                    ),
+                    CupertinoButton(
+                      onPressed: () {},
+                      minSize: 0,
+                      padding: EdgeInsets.all(10.w),
+                      child: Image.asset(
+                        "assets/images/back_white.webp",
+                        width: 20.w,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Informações pessoais",
-              style: TextStyle(fontSize: 28.w, color: Colors.white, fontWeight: FontWeight.w400),
-            ),
-            SizedBox(
-              width: 388.w,
-              child: Text(
-                "Informações pessoais completas.",
-                maxLines: 3,
-                style: TextStyle(fontSize: 24.w, color: const Color.fromRGBO(255, 255, 255, 0.70), fontWeight: FontWeight.w400),
-              ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CupertinoButton(
-              onPressed: () {},
-              minSize: 0,
-              padding: EdgeInsets.all(10.w),
-              child: Image.asset(
-                "assets/images/security_center_right_ok.webp",
-                width: 32.w,
-              ),
-            ),
-
-            CupertinoButton(
-              onPressed: () {},
-              minSize: 0,
-              padding: EdgeInsets.all(10.w),
-              child: Image.asset(
-                "assets/images/security_center_right_ok.webp",
-                width: 32.w,
-              ),
-            ),
-
-            CupertinoButton(
-              onPressed: () {},
-              minSize: 0,
-              padding: EdgeInsets.all(10.w),
-              child: Image.asset(
-                "assets/images/security_center_right_ok.webp",
-                width: 32.w,
-              ),
-            ),
-          ],
-        )
+        isLast ? const SizedBox() : const Line(),
       ],
     );
   }
