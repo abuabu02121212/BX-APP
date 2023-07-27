dc2b16dabc136082fd08b3e9f79e71ab5c38c4b3
henry <henry@gmail.com>
2023-07-27 16:19:41 +0800

    AppButton新增Disable状态

--- lib/app/component/app_button.dart
+++ lib/app/component/app_button.dart
@@ -10,6 +10,8 @@ class AppButton extends StatelessWidget {
     required this.text,
     required this.onClick,
     this.colorList = colorList1,
+    this.disableColorList = disableColorList1,
+    this.disable = false,
     required this.radius,
     this.textColor = Colors.white,
   });
@@ -19,16 +21,20 @@ class AppButton extends StatelessWidget {
   final String text;
   final VoidCallback onClick;
   final List<Color> colorList;
+  final List<Color> disableColorList;
   final double radius;
   final Color textColor;
+  final bool disable;
 
   static const colorList1 = [Color(0xff0ED1F4), Color(0xff1373EF)];
+  static const disableColorList1 = [Color(0x880ED1F4), Color(0x881373EF)];
   static const colorList2 = [Color(0xffFFD500), Color(0xffFF9901)];
+  static const disableColorList2 = [Color(0x88FFD500), Color(0x88FF9901)];
 
   @override
   Widget build(BuildContext context) {
     return CupertinoButton(
-      onPressed: onClick,
+      onPressed: disable ? null : onClick,
       minSize: 0,
       pressedOpacity: 0.8,
       padding: const EdgeInsets.all(0),
@@ -36,7 +42,7 @@ class AppButton extends StatelessWidget {
         width: width,
         height: height,
         decoration: BoxDecoration(
-          gradient:  LinearGradient(colors: colorList),
+          gradient: LinearGradient(colors: disable ? disableColorList : colorList),
           borderRadius: BorderRadius.circular(radius),
         ),
         alignment: Alignment.center,
