4c620f227c5b3c57541f26da0b3f7a765544255e
henry <henry@gmail.com>
2023-07-27 16:26:02 +0800

    disble状态优化

--- lib/app/component/app_button.dart
+++ lib/app/component/app_button.dart
@@ -10,7 +10,6 @@ class AppButton extends StatelessWidget {
     required this.text,
     required this.onClick,
     this.colorList = colorList1,
-    this.disableColorList = disableColorList1,
     this.disable = false,
     required this.radius,
     this.textColor = Colors.white,
@@ -21,15 +20,12 @@ class AppButton extends StatelessWidget {
   final String text;
   final VoidCallback onClick;
   final List<Color> colorList;
-  final List<Color> disableColorList;
   final double radius;
   final Color textColor;
   final bool disable;
 
   static const colorList1 = [Color(0xff0ED1F4), Color(0xff1373EF)];
-  static const disableColorList1 = [Color(0x880ED1F4), Color(0x881373EF)];
   static const colorList2 = [Color(0xffFFD500), Color(0xffFF9901)];
-  static const disableColorList2 = [Color(0x88FFD500), Color(0x88FF9901)];
 
   @override
   Widget build(BuildContext context) {
@@ -38,20 +34,23 @@ class AppButton extends StatelessWidget {
       minSize: 0,
       pressedOpacity: 0.8,
       padding: const EdgeInsets.all(0),
-      child: Container(
-        width: width,
-        height: height,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: disable ? disableColorList : colorList),
-          borderRadius: BorderRadius.circular(radius),
-        ),
-        alignment: Alignment.center,
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 28.w,
-            color: textColor,
-            fontWeight: FontWeight.w700,
+      child: Opacity(
+        opacity: disable ? 0.6 : 1,
+        child: Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: colorList),
+            borderRadius: BorderRadius.circular(radius),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 28.w,
+              color: textColor,
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ),
       ),
