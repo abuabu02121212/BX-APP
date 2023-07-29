2872b20a21c2f81dabe46bd5429e488d00bba43a
henry <henry@gmail.com>
2023-07-29 15:22:17 +0800

    通知接口相关字段去掉URL双侧解码

--- lib/app/entity/notice.dart
+++ lib/app/entity/notice.dart
@@ -26,8 +26,8 @@ class NoticeEntity {
   factory NoticeEntity.fromJson(Map<String, dynamic> json) {
     return NoticeEntity(
       id: json['id'].toString(),
-      title: getUrlDoubleDecodeStr(json['title'].toString()),
-      content: getUrlDoubleDecodeStr(json['content'].toString()),
+      title: json['title'].toString(),
+      content: json['content'].toString(),
       redirect: json['redirect'].toString(),
       redirectUrl: json['redirect_url'].toString(),
       state: json['state'].toString(),
