a865f14748ca99f6a71cb6a3dafbec6bb4c59a6a
less1970 <jama7488@gmail.com>
2023-07-31 14:55:42 +0800

    修复跳转webview bug

--- lib/util/weburl_util.dart
+++ lib/util/weburl_util.dart
@@ -7,50 +7,45 @@ import 'Log.dart';
 class WebURLUtil {
   static const String _BASE_URL = "https://h5.cyestari.com";
 
-  static String _getToken() {
-    String loginToken = spUtil.getString(keyLoginToken) ?? "";
-    return loginToken;
-  }
-
   /// 活动-首存
   static  Map<String ,String> ACTIVITY_DETAIL_FIRST_DEPOSIT = {
-    "url": "$_BASE_URL/promotion-detail/first-deposit?is-app=1&t=${_getToken()}",
+    "url": "$_BASE_URL/promotion-detail/first-deposit?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}",
     "title": "Primeira recarga"
   };
 
   /// 活动-充值福利
   static Map<String ,String> ACTIVITY_DETAIL_DEPOSIT_BONUS = {
-    "url": "$_BASE_URL/promotion-detail/deposit-bonus?is-app=1&t=${_getToken()}",
+    "url": "$_BASE_URL/promotion-detail/deposit-bonus?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}",
     "title": "Recarga benefícios"
   };
 
   /// 活动-签到
   static Map<String ,String> ACTIVITY_DETAIL_CHECK_IN = {
-    "url": "$_BASE_URL/promotion-detail/check-in?is-app=1&t=${_getToken()}",
+    "url": "$_BASE_URL/promotion-detail/check-in?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}",
     "title": ""
   };
 
   /// 活动-宝箱
   static Map<String ,String> ACTIVITY_DETAIL_REWARD_BOX = {
-    "url": "$_BASE_URL/promotion-detail/reward-box?is-app=1&t=${_getToken()}",
+    "url": "$_BASE_URL/promotion-detail/reward-box?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}",
     "title": "Jackpot de desempacotar"
   };
 
   /// 活动-邀请
   static Map<String ,String> ACTIVITY_DETAIL_INVITE = {
-    "url": "$_BASE_URL/promotion-detail/invite?is-app=1&t=${_getToken()}",
+    "url": "$_BASE_URL/promotion-detail/invite?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}",
     "title": "Bónus de Convite"
   };
 
   /// 活动-每周累计投注
   static Map<String ,String> ACTIVITY_REFUND = {
-    "url": "$_BASE_URL/promotion-detail/refund?is-app=1&t=${_getToken()}",
+    "url": "$_BASE_URL/promotion-detail/refund?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}",
     "title": "Apostas cumulativas semanais"
   };
 
   /// 活动Telegram
   static Map<String ,String> ACTIVITY_DETAIL_TELEGRAM = {
-    "url": "$_BASE_URL/promotion-detail/telegram?is-app=1&t=${_getToken()}",
+    "url": "$_BASE_URL/promotion-detail/telegram?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}",
     "title": "Canal De Telegram"
   };
 
@@ -61,12 +56,12 @@ class WebURLUtil {
   };
 
   static Map<String ,String> REGISTER_RULES = {
-    "url": "$_BASE_URL/privacy?is-app=1&t=${_getToken()}",
+    "url": "$_BASE_URL/privacy?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}",
     "title": "Privacy"
   };
 
   static Map<String ,String> getBannerTarPageParam(String p1){
-    String url = "$_BASE_URL$p1?is-app=1&t=${_getToken()}";
+    String url = "$_BASE_URL$p1?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}";
     Log.d("=======url:$url");
     return {
       "url": url,
@@ -75,7 +70,7 @@ class WebURLUtil {
   }
 
   static Map<String ,String> getGamePageParam(String url){
-    String url = "url?is-app=1&t=${_getToken()}";
+    String url = "url?is-app=1&t=${spUtil.getString(keyLoginToken) ?? ""}";
     Log.d("=======url:$url");
     return {
       "url": url,
